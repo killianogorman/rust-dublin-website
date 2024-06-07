1003758c45d391f480fbccd029cfc9687abaa045
Killian O'Gorman <83577130+killianogorman@users.noreply.github.com>
2024-06-07 16:37:33 +0100

    Add and configure collections and content

---
+++ src/content/config.ts
@@ -0,0 +1,42 @@
+import { z, defineCollection } from "astro:content";
+const talksCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    event_location: z.string(),
+    video_url: z.string(),
+    slides_url: z.string(),
+    tags: z.array(z.string()),
+  }),
+});
+const peopleCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    slug: z.string(),
+    name: z.string(),
+    bio: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+    urls: z.array(z.string()),
+    company_info: z.object({
+      position: z.string(),
+      name: z.string(),
+      company_bio: z.string(),
+    }),
+  }),
+});
+
+export const collections = {
+  talks: talksCollection,
+  people: peopleCollection,
+};
---
+++ src/content/people/pastuszek.md
@@ -0,0 +1,14 @@
+---
+    slug: 'pastuszek'
+    name: 'Jakub Pastuszek'
+    bio: 'I am a co-organiser of the Rust Dublin meetup where you can find me participating or presenting. I am interested in UNIX and Linux, programming, electronics and technology in general. I like coding in Rust. I used to code in Ruby, C, C++ and even in AVR assembly.I also like to think about privacy, freedom, energy and our future.'
+    image: 
+        url: 'https://avatars.githubusercontent.com/u/20052?v=4'
+        alt: 'Jakub Pastuszek'
+    tags: ["organiser","pastuszek","jakub"]
+    urls: ["https://jpastuszek.net"]
+    company_info: 
+        position: NULL
+        name: NULL
+        company_bio: NULL
+---
