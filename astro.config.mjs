139064acc79b071454a875c4c9f1edff84b6ff3c
Killian O'Gorman <83577130+killianogorman@users.noreply.github.com>
2024-06-10 10:56:27 +0100

    Update Talks Styles and Add MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: "https://astronaut.github.io",
+  integrations: [tailwind(), mdx()],
+  site: "https://astronaut.github.io"
   // base: 'rust-dublin-website',
 });
